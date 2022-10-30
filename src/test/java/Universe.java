3716d9f9bd7797a989a80ebaca55eff736c78605
qwer083 <Qwertyiop890>
2022-10-30 18:29:44 +0300

    Initial commit

---
+++ src/test/java/Universe.java
@@ -0,0 +1,8 @@
+public class Universe {public static void main(String []args) {
+    long a = 13000000000L;
+    long b = 100000;
+    long c = a/b;
+    String s = "во сколько раз Вселенная старше людей";
+    System.out.println("во сколько раз Вселенная старше людей: " + c);
+}
+}
