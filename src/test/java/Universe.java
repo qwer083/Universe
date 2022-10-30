6801839edfc03b331de97e63f2b3ab3d5f7aed80
qwer083 <Qwertyiop890>
2022-10-30 20:14:09 +0300

    final

--- src/test/java/Universe.java
+++ src/test/java/Universe.java
@@ -2,7 +2,8 @@ public class Universe {public static void main(String []args) {
     long a = 13000000000L;
     long b = 100000;
     long c = a/b;
-    String s = "во сколько раз Вселенная старше людей";
+    System.out.println("возраст Вселенной в годах: " + a);
+    System.out.println("возраст человека, как вида: " + b);
     System.out.println("во сколько раз Вселенная старше людей: " + c);
 }
 }
